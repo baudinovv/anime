33162d9b853ee86f1264f1dda3ae14b0d0d08091
Alisher <alisher.baudinovv@gmail.com>
2024-01-14 05:50:05 +0600

    fix: more faster modal on main, not on anime page

--- js/script.js
+++ js/script.js
@@ -17,3 +17,13 @@ document.getElementById("rgstr").addEventListener("click", function() {
 document.getElementById("modal-window__close-reg").addEventListener("click", function() {
     document.getElementById("modal-window-reg").classList.remove("open")
 })
+// login --> reg
+document.getElementById("login>reg").addEventListener("click", function() {
+    document.getElementById("modal-window-reg").classList.add("open")
+    document.getElementById("modal-window").classList.remove("open")
+})
+// reg --> login
+document.getElementById("reg>login").addEventListener("click", function() {
+    document.getElementById("modal-window-reg").classList.remove("open")
+    document.getElementById("modal-window").classList.add("open")
+})
